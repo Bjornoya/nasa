6f3464a482cf0e6e0806d1b77321abc80deecc07
Gleb Ambrazhevich <glebikx@gmail.com>
2022-03-20 12:22:51 +0100

    Moved `Search` and `Filters` out of `Main` to separate component

---
+++ src/pages/Main/SearchBar/SearchBar.jsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useReducer } from 'react';
+import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+import Search from 'components/Search';
+import PropTypes from 'prop-types';
+import Filters from '../Filters';
+import { initialState, reducer, regex, TYPES } from '../utils';
+
+function SearchBar(props) {
+  const { request, setData } = props;
+  const navigate = useNavigate();
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const onSearch = (e) => {
+    const {
+      target: { value },
+    } = e;
+    if (value.match(regex)) {
+      dispatch({ type: TYPES.SEARCH, payload: value });
+    }
+  };
+
+  const onSetFilters = (date, dateString) => {
+    dispatch({ type: TYPES.FILTERS, payload: { moment: date, strings: dateString } });
+  };
+
+  /* Update URL on search query and selected filters */
+  useEffect(() => {
+    const params = new URLSearchParams('media_type=image');
+    /* Manually reset the data if input is empty (BE returns an array of values even if we don't pass params) */
+    if (!state.query) {
+      setData(null);
+    }
+    if (state.query && state.dates.strings.every(Boolean)) {
+      params.append('year_start', state.dates.strings[0]);
+      params.append('year_end', state.dates.strings[1]);
+    }
+    /* We do the API call only if input isn't empty */
+    if (state.query) {
+      params.append('q', state.query);
+      request(params.toString());
+    }
+    navigate({ search: params.toString() });
+  }, [state.query, state.dates.strings, navigate]);
+
+  return (
+    <Bar>
+      <Search onChange={onSearch} value={state.query} />
+      <StyledFilters onChange={onSetFilters} value={state.dates.moment} />
+    </Bar>
+  );
+}
+
+const Bar = styled.div`
+  width: 50%;
+  margin: 0 auto;
+  text-align: right;
+`;
+
+const StyledFilters = styled(Filters)`
+  margin: 16px 0;
+`;
+
+SearchBar.propTypes = {
+  request: PropTypes.func.isRequired,
+  setData: PropTypes.func.isRequired,
+};
+
+export default SearchBar;
