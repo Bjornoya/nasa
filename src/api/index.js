a660c6073964baaac81326560afdfdec8ee4a710
Gleb Ambrazhevich <glebikx@gmail.com>
2022-03-20 13:31:17 +0100

    Created `getAssetsImage` function

--- src/api/index.js
+++ src/api/index.js
@@ -1,15 +1,24 @@
 export const ROOT_URL = 'https://images-api.nasa.gov';
+export const ROOT_ASSETS_URL = 'https://images-assets.nasa.gov';
 
 /**
  * Performing a search
  * @param {string} params - Free text search terms to compare to all indexed metadata.
- * @returns {string} - Search results API endpoint
+ * @returns {Promise} - Search results API data
  */
 export const getSearch = (params) => fetch(`${ROOT_URL}/search?${params}`);
 
 /**
  * Getting details by ID
  * @param {string} id - The media asset’s NASA ID
- * @returns {string} - Asset details API endpoint
+ * @returns {Promise} - Asset details API data
  */
 export const getDetails = (id) => fetch(`${ROOT_URL}/search?nasa_id=${id}`);
+
+/**
+ * Getting image URL by id and size
+ * @param {string} id - The media asset’s ID
+ * @param {'orig'|'large'|'medium'|'small'|'thumb'} size - Image's size
+ * @returns {string} - Image's URL
+ */
+export const getAssetsImage = (id, size) => `${ROOT_ASSETS_URL}/image/${id}/~${size}.jpg`;
